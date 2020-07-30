08a4bd33001e617b18cc54fa7b9332dc53430abd
Rajesh Raghavan <rraghavan@illumina.com>
2020-07-30 18:14:09 +0000

    Test

---
+++ Check_R_Pkgs.R
@@ -0,0 +1,9 @@
+installed.packages()[1:50,]
+installed.packages()
+
+installed.packages(lib.loc = NULL, priority = NULL,
+                   noCache = FALSE, fields = NULL,
+                   subarch = .Platform$r_arch)
+
+version
+packageStatus()
